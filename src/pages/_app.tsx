868acc844058cace8d8d3c3a226b0c217b1ab280
Abolfazl <abolfazljamshididev@gmail.com>
2023-09-24 19:24:47 +0330

    add eslint configurations for the project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import Layout from "@/components/Layout";
+import Layout from "@/ui/Layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
--- src/ui/Layout.tsx
+++ src/ui/Layout.tsx
@@ -2,11 +2,16 @@ import { Fira_Code } from "next/font/google";
 const inter = Fira_Code({ subsets: ["latin"] });
 
 function Layout({ children }: { children: React.ReactNode }) {
+  function checkhHndler() {
+    let x = 0;
+    let arr = ["x", "y"];
+  }
+
   return (
     <>
       <nav></nav>
       <main
-        className={`${inter.className} border h-full-dvh max-w-screen-xl m-auto`}
+        className={`${inter.className} border h-full-dvh max-w-screen-2xl m-auto`}
       >
         {children}
       </main>
