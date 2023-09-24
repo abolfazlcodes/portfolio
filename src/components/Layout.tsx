7106eca05cbca1bb79c8ca71c3f296941f2da840
Abolfazl <abolfazljamshididev@gmail.com>
2023-09-24 16:08:08 +0330

    add layout component to the app

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,17 @@ import { Fira_Code } from "next/font/google";
 const inter = Fira_Code({ subsets: ["latin"] });
 
 function Layout({ children }: { children: React.ReactNode }) {
-  return <div className={`${inter.className}`}>{children}</div>;
+  return (
+    <>
+      <nav></nav>
+      <main
+        className={`${inter.className} border h-full-dvh max-w-screen-xl m-auto`}
+      >
+        {children}
+      </main>
+      <footer></footer>
+    </>
+  );
 }
 
 export default Layout;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      height: {
+        "full-dvh": "100dvh",
+      },
     },
   },
   plugins: [],
