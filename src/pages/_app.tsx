590cdccdf8cab60612d6cb8342b62260687c394b
Abolfazl <abolfazljamshididev@gmail.com>
2023-09-24 19:53:44 +0330

    add prettier and format script to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import Layout from "@/ui/Layout";
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import Layout from '@/ui/Layout';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- src/ui/Layout.tsx
+++ src/ui/Layout.tsx
@@ -1,17 +1,14 @@
-import { Fira_Code } from "next/font/google";
-const inter = Fira_Code({ subsets: ["latin"] });
+import { Fira_Code } from 'next/font/google';
+const inter = Fira_Code({
+  subsets: ['latin'],
+});
 
 function Layout({ children }: { children: React.ReactNode }) {
-  function checkhHndler() {
-    let x = 0;
-    let arr = ["x", "y"];
-  }
-
   return (
     <>
       <nav></nav>
       <main
-        className={`${inter.className} border h-full-dvh max-w-screen-2xl m-auto`}
+        className={`${inter.className} m-auto h-full-dvh max-w-screen-2xl border`}
       >
         {children}
       </main>
