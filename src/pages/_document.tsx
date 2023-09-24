3679bfc8d56130b5af64cb71500b0b7e527ff258
Abolfazl <abolfazljamshididev@gmail.com>
2023-09-24 20:10:51 +0330

    add google analytics to the portfolio website

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,23 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head />
+      <Head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <Script id='google-analytics'>
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
+        `}
+        </Script>
+      </Head>
       <body>
         <Main />
         <NextScript />
