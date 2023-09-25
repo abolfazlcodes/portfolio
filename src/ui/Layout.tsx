69131838dc810e7844a6cd98a2f1d15f66d13e5b
Abolfazl <abolfazljamshididev@gmail.com>
2023-09-25 15:58:36 +0330

    add sample test for home page

--- src/ui/Layout.tsx
+++ src/ui/Layout.tsx
@@ -1,1 +1,20 @@
-import { Fira_Code } from 'next/font/google';const inter = Fira_Code({  subsets: ['latin'],});function Layout({ children }: { children: React.ReactNode }) {  return (    <>      <nav></nav>      <main        className={`${inter.className} m-auto h-full-dvh max-w-screen-2xl border`}      >        {children}      </main>      <footer></footer>    </>  );}export default Layout;
+import { Fira_Code } from 'next/font/google';
+const inter = Fira_Code({
+  subsets: ['latin'],
+});
+
+function Layout({ children }: { children?: React.ReactNode }) {
+  return (
+    <>
+      <nav></nav>
+      <main
+        className={`${inter.className} m-auto h-full-dvh max-w-screen-2xl border`}
+      >
+        {children}
+      </main>
+      <footer></footer>
+    </>
+  );
+}
+
+export default Layout;
