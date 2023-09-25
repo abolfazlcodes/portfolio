e5b59abbe58ccd76e17e17d602739fe2eea406bc
Abolfazl <abolfazljamshididev@gmail.com>
2023-09-25 23:44:54 +0330

    update header navigation border color

--- src/ui/ListItem.tsx
+++ src/ui/ListItem.tsx
@@ -9,7 +9,7 @@ interface ListItemProps {
 function ListItem({ link, title, index }: ListItemProps) {
   return (
     <li
-      className={` group relative flex items-center justify-center border-[#607B96] px-10 py-6 text-xl font-medium duration-200 hover:text-[#FFF] ${
+      className={` group relative flex items-center justify-center border-[#1E2D3D] px-10 py-6 text-xl font-medium duration-200 hover:text-[#FFF] ${
         index >= 1 ? 'border-r' : 'border-x'
       } `}
     >
--- src/ui/Navbar.tsx
+++ src/ui/Navbar.tsx
@@ -4,12 +4,12 @@ import Logo from './Logo';
 
 function Navbar() {
   return (
-    <nav className='flex items-center justify-between border-b border-[#607B96] px-4'>
+    <nav className='flex items-center justify-between border-b border-[#1E2D3D] px-4'>
       <div className='flex w-11/12 items-center'>
         <Logo />
         <ListWrapper />
       </div>
-      <div className='group relative w-1/12 justify-end border-l border-[#607b96]'>
+      <div className='group relative w-1/12 justify-end border-l border-[#1E2D3D]'>
         <Link
           href='/contact-me'
           className='h-18 flex w-full items-center justify-center text-xl font-medium duration-200 hover:text-[#FFF]'
