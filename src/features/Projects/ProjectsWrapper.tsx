7a58fcdfba91aebaddd9dbc785d37954af7b3cd9
Abolfazl <abolfazljamshididev@gmail.com>
2023-10-01 15:37:07 +0330

    add video of the projects and play and pause when maouse enters or leaves

--- src/features/Projects/ProjectsWrapper.tsx
+++ src/features/Projects/ProjectsWrapper.tsx
@@ -1,5 +1,4 @@
 import dynamic from 'next/dynamic';
-import sampleProject from '../../../public/sample project.png';
 
 const DynamicProjectsCard = dynamic(
   () => import('../../ui/ProjectCard'),
@@ -12,22 +11,19 @@ const DynamicProjectsCard = dynamic(
 const projectsFakeData = [
   {
     id: 1,
-    image: sampleProject,
-    title: '_ui-animations',
+    title: '_hotel-dashboard',
     description:
       'Duis aute irure dolor in velit esse cillum dolore.',
   },
   {
     id: 2,
-    image: sampleProject,
-    title: '_tetris-game',
+    title: '_car-rent-platform',
     description:
       'Duis aute irure dolor in velit esse cillum dolore.',
   },
   {
     id: 3,
-    image: sampleProject,
-    title: '_ethereum',
+    title: '_hotel-dashboard',
     description:
       'Duis aute irure dolor in velit esse cillum dolore.',
   },
@@ -45,7 +41,7 @@ function ProjectsWrapper() {
           </div>
         </header>
 
-        <div className='flex h-full flex-wrap justify-center gap-8 p-10 sm:py-48'>
+        <div className='flex h-full flex-wrap items-center justify-center gap-8 p-10'>
           <DynamicProjectsCard card={projectsFakeData[0]} />
           <DynamicProjectsCard card={projectsFakeData[1]} />
           <DynamicProjectsCard card={projectsFakeData[2]} />
--- src/ui/ProjectCard.tsx
+++ src/ui/ProjectCard.tsx
@@ -1,22 +1,38 @@
-import Image, { StaticImageData } from 'next/image';
-
 import Button from './Button';
+import { useState, MouseEvent } from 'react';
 
 interface ProjectCardProps {
   card: {
     title: string;
     id: number;
-    image: StaticImageData | string;
     description: string;
   };
 }
 
 function ProjectCard({ card }: ProjectCardProps) {
-  const { title, id, image, description } = card;
+  const { title, id, description } = card;
+  const [shouldPlay, setShouldPlay] = useState(false);
+
+  const mouseEnterHandler = (
+    e: MouseEvent<HTMLVideoElement>
+  ) => {
+    if (!shouldPlay) e.currentTarget.play();
+
+    setShouldPlay(true);
+  };
+
+  const mouseLeaveHandler = (
+    e: MouseEvent<HTMLVideoElement>
+  ) => {
+    if (shouldPlay) e.currentTarget.pause();
+    setShouldPlay(false);
+  };
+
+  const video = id % 2 === 0 ? '/morent.mp4' : '/hotel.mp4';
 
   /* eslint-disable react/jsx-no-comment-textnodes */
   return (
-    <article className='flex flex-col gap-6'>
+    <article className='flex h-[45rem] w-[40rem] cursor-pointer flex-col gap-6 duration-200 hover:-translate-y-3'>
       <header className='flex items-center gap-3'>
         <h1 className='text-2xl font-bold capitalize text-[#5565E8]'>
           Project {id}
@@ -25,16 +41,18 @@ function ProjectCard({ card }: ProjectCardProps) {
           // {title}
         </span>
       </header>
-      <div className='w-112 h-106 rounded-3xl border border-[#1E2D3D]'>
-        <Image
-          src={image}
-          width={0}
-          height={0}
-          sizes='100vw'
-          className='h-auto w-full rounded-lg object-cover'
-          alt=''
-        />
-        <div className='flex flex-col gap-8 border-t border-[#1e2d3d] p-6'>
+      {/*  */}
+      <div className='flex h-full w-full flex-col overflow-hidden rounded-3xl border border-[#1E2D3D]'>
+        <div className='flex-1'>
+          <video
+            src={video}
+            muted
+            loop
+            onMouseEnter={(e) => mouseEnterHandler(e)}
+            onMouseLeave={(e) => mouseLeaveHandler(e)}
+          />
+        </div>
+        <div className='flex flex-col gap-8 p-6'>
           <p className='text-xl text-[#607b96]'>{description}</p>
           <Button>view-project</Button>
         </div>
