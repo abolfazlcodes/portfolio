a2d782b185236ac7b9845d432dcb0963cea32502
Abolfazl <abolfazljamshididev@gmail.com>
2023-10-01 08:42:06 +0330

    add secrets to ci file

--- src/features/Contact/Contact.tsx
+++ src/features/Contact/Contact.tsx
@@ -40,7 +40,7 @@ function Contact() {
     e.preventDefault();
 
     // TODO: handle contact form submission
-    fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/contacts`, {
+    fetch(`/api/contacts`, {
       method: 'POST',
       headers: {
         Accept: 'application/json, text/plain, */*',
