38ffa30f3a8d36cffa76e7bc87d80c2d796dbe26
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2023-11-15 22:18:38 +0330

    interlink contact page with about me page

--- src/ui/SidebarContacts.tsx
+++ src/ui/SidebarContacts.tsx
@@ -1,7 +1,11 @@
 import { EmailIcon, PhoneIcon } from '@/utils/iconsHelper';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 function SidebarContacts() {
+  const { pathname } = useRouter();
+  const isContactActive = pathname === '/contact-me';
+
   return (
     <article
       className={`hidden w-full border-x border-[#1E2D3D] px-1 xl:block`}
@@ -17,9 +21,15 @@ function SidebarContacts() {
         <PhoneIcon />
         <p>+989223110962</p>
       </div>
-      <div className='mb-3 flex items-center gap-3 px-2 text-xl'>
-        <Link href='/about-me'>read about me</Link>
-      </div>
+      {!!isContactActive ? (
+        <div className='mb-3 flex items-center gap-3 px-2 text-xl'>
+          <Link href='/about-me'>read about me</Link>
+        </div>
+      ) : (
+        <div className='mb-3 flex items-center gap-3 px-2 text-xl'>
+          <Link href='/contact-me'>get in touch with me</Link>
+        </div>
+      )}
     </article>
   );
 }
